a9d1833cc58fe1ef270c87484a523f12dbed32f2
Matt Gordon <bigmattsize21@gmail.com>
2020-04-02 21:09:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,10 +23,14 @@ class Artist
   def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.new(name)
   end
+  
   def self.find(name)
     self.all.find {|artist| artist.name == name }
   end
+  
   def self.create(name)
     self.new(name).tap {|artist| artist.save}
   end
+  
+  
 end
